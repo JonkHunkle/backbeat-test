59504097548611773404c257ee206c8a68aeb894
dczer <czerwinskidavid1@gmail.com>
2023-05-29 16:52:36 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,43 +9,33 @@ let baseURL = "https://mbardin.github.io/PDM-resources/media/sound_samples";
 function App() {
   const [playing, setPlaying]= useState(false)
   const [selectedBackbeat, setSelectedBackbeat]= useState('')
-  const [tracks, setTracks] = useState(undefined)
   const [loop, setLoop]=useState(undefined)
-  const [transport, setTransport]=useState(undefined)
-  const [drumSet, setDrumSet] = useState(undefined)
-  const [snare, setSnare]= useState(undefined)
-  const [hiHat, setHiHat]= useState(undefined)
-  const [kick, setKick]= useState(undefined)
 const drumSetRef=useRef(undefined)
 const backbeatRef=useRef(undefined)
 const counterRef= useRef(0)
   const drumRef= useRef({
     rock:{
       hh:[1,0,1,0,1,0,1,0],
-      kd:[1,0,0,0,1,0,0,1],
+      kd:[1,1,0,1,1,1,0,1],
       sn:[0,0,1,0,0,0,1,0]
     },
     blues:{
-      hh:[1,0,0,0,1,0,0,1],
-      kd:[1,0,0,0,0,0,0,0],
+      hh:[1,0,0,0,1,0,1,1],
+      kd:[1,0,0,0,0,0,0,1],
       sn:[0,0,0,0,1,0,0,0]
     }
   })
   
-  const playSong= (time)=>{
-      counterRef.current = (counterRef.current +1)%division
-      playInstrument(time)
-  }
-
-  let playInstrument = (time)=>{
-
-      for (const instrument in drumRef.current[backbeatRef.current]){
-        drumRef.current[backbeatRef.current][instrument].forEach((beat, i) => {
-          if(beat>0 && i===counterRef.current){
-            drumSetRef.current[instrument].triggerAttackRelease('C2','16n', time,1);
-          }
-        })
-}
+  let play = (time)=>{
+    for (const instrument in drumRef.current[backbeatRef.current]){
+      drumRef.current[backbeatRef.current][instrument].forEach((beat, i) => {
+        console.log(beat,i)
+        if(beat>0 && i===counterRef.current){
+          drumSetRef.current[instrument].triggerAttackRelease('C2','32n', time,1);
+        }
+      })
+    }
+    counterRef.current = (counterRef.current +1)%division
 }
   const startBeat = async(e)=>{
     e.preventDefault()
@@ -53,9 +43,12 @@ const counterRef= useRef(0)
       if(!playing){
         await Tone.loaded()
         Tone.Transport.start();
-        loop.start(0);
+        loop.start();
       }else{
+        // Tone.Transport.stop();
         loop.stop();
+        counterRef.current=0
+
       }
       setPlaying(!playing)
     }
@@ -65,7 +58,7 @@ const selectBackbeat= (e)=>{
   e.preventDefault()
   const {value} = e.target
   backbeatRef.current=value
-  loop.stop()
+  loop.stop(0)
   setPlaying(false)
   }
   Tone.Transport.bpm.value=120
@@ -73,17 +66,19 @@ const selectBackbeat= (e)=>{
 
   useEffect(()=>{
   
-  if(drumSet===undefined){
+  if(drumSetRef.current===undefined){
     let newDrum= {}
     let hiHat= new Tone.Sampler({
       urls: { C2: `hat.mp3` },
       baseUrl:
           'https://res.cloudinary.com/dcttcffbc/video/upload/v1597045705/samples/react-sequencer/',
   }).toDestination();
+
     let kick= new Tone.Sampler({
       urls: { C2: '/perc/Kick01.mp3' },
       baseUrl:baseURL,
   }).toDestination();
+
     let snare= new Tone.Sampler({
       urls: { C2: 'snare' },
       baseUrl:'https://res.cloudinary.com/dcttcffbc/video/upload/v1597045705/samples/react-sequencer/',
@@ -95,7 +90,7 @@ const selectBackbeat= (e)=>{
     drumSetRef.current=newDrum
   }
 
-  if(loop===undefined) setLoop(new Tone.Loop(playSong, '16n'))
+  if(loop===undefined) setLoop(new Tone.Loop(play, '16n'))
 
   },[])
 
@@ -108,7 +103,7 @@ const selectBackbeat= (e)=>{
         <option value='rock'>rock</option>
         <option value='blues'>blues</option>
       </select>
-       <button onClick={startBeat} >Start the {selectedBackbeat} beat!</button>
+       <button onClick={startBeat}> {!playing?'Start':'Stop'} the {selectedBackbeat} beat!</button>
        <p>{`${playing}`}</p>
       </div>
     </div>
